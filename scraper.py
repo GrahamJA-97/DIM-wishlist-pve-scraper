c9b1041323864b06ef898609713bbc41251b8950
JoshSchmucki <joshuaschmucki@gmail.com>
2022-12-24 22:36:28 -0500

    Add python version

--- scraper.py
+++ scraper.py
@@ -1,58 +1,48 @@
 """
 This file will handle the business logic required to perform the scraping needed to form the new txt file.
 """
-import os
-import urllib.request
-
 import requests
 
-# CONSTANTS
-URL_TO_SCRAPE = "https://raw.githubusercontent.com/48klocs/dim-wish-list-sources/master/voltron.txt"
-SMALL_FILE_NAME = os.path.join('local-sample', 'small.txt')
-FULL_FILE_NAME = os.path.join('local-sample', 'voltron.txt')
+# Constants
+PATH = ""
+VOLTRON_URL = "https://raw.githubusercontent.com/48klocs/dim-wish-list-sources/master/voltron.txt"
+SMALL_URL = "https://raw.githubusercontent.com/GrahamJA-97/DIM-wishlist-pve-scraper/python/local-samples/small.txtxt"
 
+def do_thing(shouldFilter):
+    response = requests.get(VOLTRON_URL, stream=True)
 
-# function for testing the process to see if it works on small local files before scaling up to the web version
-def lines_from_file(file_name):
-    print("File name: " + file_name + "\n")
-    with open(file_name, "r") as file:
-        print("Exists: " + file)
-        for line in file:
-            print(line)
+    if shouldFilter:
+        filtered_result(response)
+    else:
+        full_list(response)
 
+def full_list(response):
+    output_file = open(PATH + "output.txt", "w")
+    for line in response.iter_lines():
+        output_file.write(line.decode("utf-8") + "\n")
 
-def lines_from_urllib(url):
-    response = urllib.request.urlopen(url)
-    return response.readlines()
+    output_file.close()
 
+def filtered_result(response):
+    output_file = open(PATH + "filtered_result.txt", "w")
+    found_pvp = False
 
-def process_with_request(url):
-    request = requests.get(url)
-    # if request != requests.codes.ok:
-    #     print("Something wrong with request, try again later")
-    #     return
+    for line in response.iter_lines():
+        decoded = line.decode("utf-8")
+        lower = decoded.lower()
+        if (not found_pvp) and ("pvp" in lower) and (not "pve" in lower):
+            found_pvp = True
+        elif not line:
+            found_pvp = False
 
-    for line in request.text:
-        print(line)
-        # if "pvp" in line.lower():
-        #     print(line)
+        if not found_pvp:
+            output_file.write(decoded + "\n")
 
+    output_file.close()
 
 if __name__ == '__main__':
     print("~~~~~Start of file reading~~~~~")
-    # with open("output.txt", 'w') as output_file:
-    lines = lines_from_urllib(URL_TO_SCRAPE)
-    found_pvp = False
 
-    for line in lines:
-        string = line.decode()
-
-        if "PvP" in string:
-            found_pvp = True
-            print("~~~~~~~~~~~~" + string)
-        # elif string == '\n':
-        #     found_pvp = False
-        else:
-            print(string)
+    do_thing(True)
 
     print("~~~~~End of file reading~~~~~")
