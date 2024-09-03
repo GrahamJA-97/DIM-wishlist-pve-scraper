3453f682b6496fd1d76fe77c515e95228907d612
Jake Graham <51917743+GrahamJA-97@users.noreply.github.com>
2024-09-03 11:33:36 -0400

    Remove java from main branch

--- run_java.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Bash script to compile & run java implementation"
-
-cd java/src
-javac Main.java && java Main
-
-echo "Move file to top level location"
-mv voltron-pve-filter.txt ../..
