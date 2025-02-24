f1eb5582cbb80662ba8d639d9e807a648dfe63ce
Jake Graham <51917743+GrahamJA-97@users.noreply.github.com>
2025-02-24 14:17:11 -0500

    Update the script
    Has more aggressive filtering now by purging duplicates and a new custom title & description

--- pve-scrape.py
+++ pve-scrape.py
@@ -14,20 +14,44 @@ def filter_pvp_blocks(url, output_file):
     blocks = data.split('\n\n')
 
     filtered_blocks = []
+    seen_wishlist_items = set()
 
     for block in blocks:
         # Skip blocks that contain "PvP", unless they mention "PvE" (case-insensitive)
         if re.search(r'pvp', block, re.IGNORECASE) and not re.search(r'pve', block, re.IGNORECASE):
             continue
 
+        # Process block lines to remove duplicate dimwishlist:item= entries
+        lines = block.split('\n')
+        unique_lines = []
+        contains_wishlist_item = False
+
+        for line in lines:
+            if line.startswith("dimwishlist:item="):
+                if line in seen_wishlist_items:
+                    continue
+                seen_wishlist_items.add(line)
+                contains_wishlist_item = True
+            unique_lines.append(line)
+
+        # Skip block if no item roll lines remain
+        if not contains_wishlist_item:
+            continue
+
         # Add the cleaned block to the filtered list
-        filtered_blocks.append(block.strip())
+        filtered_blocks.append('\n'.join(unique_lines).strip())
 
     # Join the filtered blocks and write to the output file
     cleaned_data = '\n\n'.join(filtered_blocks).strip()
 
+    # Custom title and description lines to add to the output file
+    custom_lines = "title:This is a filtered down version of voltron.txt, a compiled collection of god/recommended rolls from top community minds.\n" + \
+                   "description:This has been filtered down to remove duplicate rolls and rip out any PVP specific rolls, updated when I think to and dependent on its source (voltron.txt).\n"
+
+    # Write to the output file with custom lines at the top
     with open(output_file, 'w') as outfile:
-        outfile.write(cleaned_data)
+        outfile.write(custom_lines + '\n' + cleaned_data)
+
 
 # Specify the URL and output file
 url = 'https://raw.githubusercontent.com/48klocs/dim-wish-list-sources/master/voltron.txt'
